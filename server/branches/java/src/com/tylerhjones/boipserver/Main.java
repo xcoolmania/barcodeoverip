3e884ee79fd665bcab1f8cdeecec4f412412951d
inquirewue <inquirewue@2cfd4830-b62d-f528-498d-5ab4222c3a2c>
2012-02-26 11:48:17 +0000

    Did a lot of work on tje Java server

--- server/branches/java/src/com/tylerhjones/boipserver/Main.java
+++ server/branches/java/src/com/tylerhjones/boipserver/Main.java
@@ -5,25 +5,99 @@
 
 package com.tylerhjones.boipserver;
 
-import javax.swing.JFrame;
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
-/**
+/*
  *
  * @author tyler
  */
 public class Main {
+    private static ServerCore CORE;
+    private static MainFrame SETS;
+    private static boolean isSysTray = true;
 
     /**
      * @param args the command line arguments
      */
 
     public static void main(String[] args) {
-        ServerCore CORE = new ServerCore();
-        JFrame SETS = new MainFrame(CORE);
-        CORE.run(SETS);
-       // SETS.setCore(CORE);
+        CORE = new ServerCore();
+        SETS = new MainFrame(CORE);
+        CORE.setWindow(SETS);
+        //SETS.setCore(CORE);
+
+        //Catch the 'X' button being pressed on the main window
+        SETS.addWindowListener(new WindowAdapter() {
+            @Override
+	    public void windowClosing(WindowEvent e) {
+                if(!isSysTray) {
+                    SETS.setVisible(false);
+                    SETS.dispose();
+                    System.exit(0);
+                } else {
+                    SETS.setVisible(false);
+                }
+	    }
+            @Override
+	    public void windowIconified(WindowEvent e) {
+	    	SETS.setVisible(false);
+	    }
+	});
+
         SETS.show();
-        // TODO code application logic here
+        //------------------------------------------------------
+        //--- Setup System Tray Icon
+        //final TrayIcon trayIcon;
+
+        if (SystemTray.isSupported()) { //Check if the system can use a systray icon
+            ImageIcon icon = createImageIcon("/icon.png", "BarcodeOverIP App Icon"); // ./build/classes/icon.png
+
+            TrayIcon tray = new TrayIcon(icon.getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT), "BarcodeOverIP-Server - Starting Up");
+            tray.addMouseListener(new MouseListener(){
+                public void mouseClicked(MouseEvent e) {
+                    if(SETS.isVisible()) {
+                        SETS.setVisible(false);
+                    } else {
+                        SETS.setVisible(true);
+                    }
+                }
+                public void mouseEntered(MouseEvent e) {}
+                public void mouseExited(MouseEvent e) {}
+                public void mousePressed(MouseEvent e) {}
+                public void mouseReleased(MouseEvent e) {}
+            });
+
+            try {
+                SystemTray.getSystemTray().add(tray);
+                SETS.setTrayIcon(tray);
+            } catch (AWTException e) {
+                System.err.println("Error adding system-tray icon!");
+                e.printStackTrace();
+            }
+        } else {
+            int n = JOptionPane.showConfirmDialog(SETS, "No system-tray was found or your system does not support one. You must NOT close the settings window!", "No System-tray!", JOptionPane.OK_OPTION);
+            isSysTray = false;
+        }
+    }
+
+    private static ImageIcon createImageIcon(String path,
+                                           String description) {
+        java.net.URL imgURL = Main.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find image: " + path);
+            return null;
+        }
     }
 
 }
--- server/branches/java/src/com/tylerhjones/boipserver/ServerCore.java
+++ server/branches/java/src/com/tylerhjones/boipserver/ServerCore.java
@@ -6,23 +6,79 @@
 package com.tylerhjones.boipserver;
 
 import java.awt.AWTException;
-import javax.swing.JFrame;
+import java.awt.MediaTracker;
 import java.awt.Robot;
-import java.awt.event.InputEvent;
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 /**
  *
  * @author tyler
  */
-public class ServerCore implements Callable {
-    private static JFrame SETS;
+public class ServerCore {
+    private static final String TAG = "ServerCore";
+    private static MainFrame MAINWIN;
+    private static Settings SETS = new Settings();
+    private static int MaxConns = 5;
+    private boolean stopNOW = false;
+    private static Toolkit toolkit;
+    private static MediaTracker tracker;
 
     public ServerCore() {
+        //MAINWIN.LogI(TAG, "Class constructor initialized!");
        // parent = p;
     }
 
+    public void start() {
+        int i = 0;
+        try{
+            //ServerSocket listener = new ServerSocket(SETS.getPort(), 2, InetAddress.getByName(SETS.getHost()));
+            ServerSocket listener = new ServerSocket(SETS.getPort());
+            Socket server;
+
+            while((i++ < MaxConns) || (MaxConns == 0)){
+                if(stopNOW) {
+                    stopNOW = false;
+                    return;
+                }
+                //ConnectionHandler CON;
+                server = listener.accept();
+                MAINWIN.LogI(TAG, "Connection recieved from: " + server.getLocalAddress().toString());
+                ConnectionHandler CONN_C = new ConnectionHandler(server, MAINWIN);
+                Thread t = new Thread(CONN_C);
+                t.start();
+            }
+        } catch (IOException ioe) {
+            System.out.println("IOException on socket listen: " + ioe);
+            ioe.printStackTrace();
+        }
+    }
+
     public void ActivateServer() {
-        SETS.setTitle("Activate WORKS!");
+        MAINWIN.LogI(TAG, "Activating server...");
+        stopNOW = false;
+        this.start();
+    }
+    public void DeactivateServer() {
+        MAINWIN.LogI(TAG, "Deactivating server...");
+        stopNOW = true;
+    }
+
+    // Sets the class variable for the MainWindow of the application
+    public void setWindow(MainFrame s) {
+        MAINWIN = s;
+    }
+
+    // Emulate the typing of the keyboard and type out a given string
+    public void TypeChars(String chars) {
+        // Verify that all the chars intending to be typed are ONLY letters and numbers.
+        if(!chars.matches("^[a-zA-Z0-9]+$")) {
+            MAINWIN.LogE(TAG, "The data sent to the server contains illegal characters!");
+            return;
+        }
+
         try {
             Robot robot = new Robot();
 
@@ -37,29 +93,4 @@ public class ServerCore implements Callable {
             e.printStackTrace();
         }
     }
-    public void DeactivateServer() {
-        SETS.setTitle("Deactivate WORKS!");
-        try {
-            Robot robot = new Robot();
-
-            // Simulate a mouse click
-            //robot.mousePress(InputEvent.BUTTON2_MASK);
-            //robot.mouseRelease(InputEvent.BUTTON2_MASK);
-
-            // Simulate a key press
-            robot.keyPress(KeyEvent.VK_B);
-            robot.keyRelease(KeyEvent.VK_B);
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-    }
-    public void CallbackMessage() {
-        SETS.hide();
-    }
-
-    public void run(JFrame s) {
-        SETS = s;
-    }
-
-
 }
