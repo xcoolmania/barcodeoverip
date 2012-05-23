c30f931aa156a497d98be2bf70c0972b23d24f10
inquirewue <inquirewue@2cfd4830-b62d-f528-498d-5ab4222c3a2c>
2012-05-23 01:18:17 +0000

    Finished designing widget. Working on widget config form now...

--- trunk/client/trunk/src/com/tylerhjones/boip/client1/BoIPWidgetConfigure.java
+++ trunk/client/trunk/src/com/tylerhjones/boip/client1/BoIPWidgetConfigure.java
@@ -26,7 +26,92 @@
 
 package com.tylerhjones.boip.client1;
 
-public class BoIPWidgetConfigure {
 
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class BoIPWidgetConfigure extends Activity {
+	
+	static final String TAG = "BoIPWidgetConfigure";
+	
+	int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+	
+	Button
+	
+	public BoIPWidgetConfigure() {
+		super();
+	}
+	
+	@Override
+	public void onCreate(Bundle wprefs) {
+		super.onCreate(wprefs);
+		
+		// Set the result to CANCELED. This will cause the widget host to cancel
+		// out of the widget placement if they press the back button.
+		setResult(RESULT_CANCELED);
+
+		setContentView(R.layout.widget_config);
+		
+		
+		Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        // If they gave us an intent without the widget id, just bail.
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+        }
+		
+	}
+	
+	View.OnClickListener mOnClickListener = new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			final Context context = BoIPWidgetConfigure.this;
+			
+			// When the button is clicked, save the string in our prefs and return that they
+			// clicked OK.
+			String titlePrefix = mAppWidgetPrefix.getText().toString();
+			saveTitlePref(context, mAppWidgetId, titlePrefix);
+			
+			// Push widget update to surface with newly set prefix
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+			BoIPWidgetProvider.updateAppWidget(context, appWidgetManager, mAppWidgetId, titlePrefix);
+			
+			// Make sure we pass back the original appWidgetId
+			Intent resultValue = new Intent();
+			resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+			setResult(RESULT_OK, resultValue);
+			finish();
+		}
+	};
+	
+	// Write the prefix to the SharedPreferences object for this widget
+	static void saveTitlePref(Context context, int appWidgetId, String text) {
+		SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+		prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
+		prefs.commit();
+	}
+	
+	// Read the prefix from the SharedPreferences object for this widget.
+	// If there is no preference saved, get the default from a resource
+	static String loadTitlePref(Context context, int appWidgetId) {
+		SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+		String prefix = prefs.getString(PREFS_KEY + appWidgetId, null);
+		if (prefix != null) {
+			return prefix;
+		} else {
+			return context.getString(R.string.appwidget_prefix_default);
+		}
+	}
 	
 }
